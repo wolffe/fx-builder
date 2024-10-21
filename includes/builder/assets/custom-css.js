4e0a281d057ca847b5996e53d223d2ac27f512b2
Ciprian Popescu <wolffe@users.noreply.github.com>
2024-10-21 21:16:13 +0100

    Remove tabbing capability in textarea element

--- includes/builder/assets/custom-css.js
+++ includes/builder/assets/custom-css.js
@@ -26,26 +26,4 @@ jQuery(document).ready(function ($) {
         $('.fxb-custom-css').hide();
         $('.fxb-modal-overlay').hide();
     });
-
-    /**
-     * ENABLE TAB IN CUSTOM CSS TEXTAREA
-     * @link http://stackoverflow.com/questions/6637341
-     */
-    $(document.body).delegate('.fxb-custom-css-textarea', 'keydown', function (e) {
-        var keyCode = e.keyCode || e.which;
-        if (keyCode == 9) {
-            e.preventDefault();
-            var start = $(this).get(0).selectionStart;
-            var end = $(this).get(0).selectionEnd;
-
-            /* set textarea value to: text before caret + tab + text after caret */
-            $(this).val($(this).val().substring(0, start)
-                + "\t"
-                + $(this).val().substring(end));
-
-            /* put caret at right position again */
-            $(this).get(0).selectionStart =
-                $(this).get(0).selectionEnd = start + 1;
-        }
-    });
 });
