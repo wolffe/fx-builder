186f7dfe8d7ed35b0079dd568883cc748f87c2e6
Ciprian Popescu <wolffe@users.noreply.github.com>
2024-10-21 18:57:02 +0100

    Add Google fonts and Bunny fonts functionality to TinyMCE's `fontformats` array

--- includes/editor.php
+++ includes/editor.php
@@ -17,10 +17,13 @@ add_filter(
         array_unshift( $buttons, 'fontselect' ); // Font family dropdown
         array_unshift( $buttons, 'fontsizeselect' );
 
-        // Used for different versions of TinyMCE
-        // Keeping them here for reference and further updates
-        //array_unshift( $buttons, 'fontsize_formats' );
-        //array_unshift( $buttons, 'fontsize' );
+        /**
+         * Compatibility: The arrays below are used for different versions of TinyMCE.
+         * Keep them here for reference and further updates.
+         *
+         * array_unshift( $buttons, 'fontsize_formats' );
+         * array_unshift( $buttons, 'fontsize' );
+         */
 
         return $buttons;
     }
@@ -38,47 +41,6 @@ add_filter(
     }
 );
 
-/*
-add_filter(
-    'tiny_mce_before_init',
-    function( $init_array ) {
-        $init_array['fontsizeselect'] = "9px 10px 12px 13px 14px 16px 18px 21px 24px 28px 32px 37px";
-
-        return $init_array;
-    }
-);
-add_filter(
-    'tiny_mce_before_init',
-    function( $init_array ) {
-        $init_array['fontsize'] = "9px 10px 12px 13px 14px 16px 18px 21px 24px 28px 32px 38px";
-
-        return $init_array;
-
-    }
-);
-/**/
-
-// Add custom Fonts to the Fonts list. Disabled for now.
-/*
-add_filter(
-    'tiny_mce_before_init',
-    function( $initArray ) {
-        $initArray['font_formats'] = 'LOL=LOL;Space+Grotesk=Space+Grotesk;Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats';
-
-        return $initArray;
-    }
-);
-
-// Add Google Scripts for use with the editor.
-add_action(
-    'admin_init',
-    function() {
-        $font_url = 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap';
-
-        add_editor_style( str_replace( ',', '%2C', $font_url ) );
-    }
-);
-/**/
 
 
 // Add new styles to the TinyMCE "formats" menu dropdown.
@@ -167,7 +129,22 @@ add_filter(
 add_filter(
     'tiny_mce_before_init',
     function ( $init_array ) {
-        $init_array['font_formats'] = 'System UI=-apple-system, BlinkMacSystemFont, Segoe UI Variable Text, Segoe UI, Roboto, Helvetica, Helvetica Neue, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Arial, sans-serif, Apple Color Emoji, Twemoji Mozilla, Segoe UI Emoji, Android Emoji;Segoe UI=Segoe UI;Segoe UI Variable Text=Segoe UI Variable Text;Segoe UI Variable Heading=Segoe UI Variable Heading;Arial=arial,helvetica,sans-serif;Georgia=georgia,palatino;Helvetica=helvetica;Times New Roman=times new roman,times;Monospace=ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace';
+        $init_array['font_formats'] = 'System UI=-apple-system, BlinkMacSystemFont, Segoe UI Variable Text, Segoe UI, Roboto, Helvetica, Helvetica Neue, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Arial, sans-serif, Apple Color Emoji, Twemoji Mozilla, Segoe UI Emoji, Android Emoji;Segoe UI=Segoe UI;Segoe UI Variable Text=Segoe UI Variable Text;Segoe UI Variable Heading=Segoe UI Variable Heading;Arial=arial,helvetica,sans-serif;Georgia=georgia,palatino;Helvetica=helvetica;Times New Roman=times new roman,times;Monospace=ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace;';
+
+        $fxb_google_fonts = (array) get_option( 'fxb_google_fonts' );
+        $fxb_bunny_fonts  = (array) get_option( 'fxb_bunny_fonts' );
+
+        if ( ! empty( $fxb_google_fonts ) ) {
+            foreach ( $fxb_google_fonts as $font ) {
+                $init_array['font_formats'] .= '[Google Fonts] ' . $font . '=' . $font . ';';
+            }
+        }
+
+        if ( ! empty( $fxb_bunny_fonts ) ) {
+            foreach ( $fxb_bunny_fonts as $font ) {
+                $init_array['font_formats'] .= '[Bunny Fonts] ' . $font . '=' . $font . ';';
+            }
+        }
 
         return $init_array;
     }
@@ -191,3 +168,41 @@ add_filter(
         return $buttons;
     }
 );
+
+
+
+/*
+add_filter(
+    'tiny_mce_before_init',
+    function( $init_array ) {
+        $init_array['fontsizeselect'] = "9px 10px 12px 13px 14px 16px 18px 21px 24px 28px 32px 37px";
+
+        return $init_array;
+    }
+);
+add_filter(
+    'tiny_mce_before_init',
+    function( $init_array ) {
+        $init_array['fontsize'] = "9px 10px 12px 13px 14px 16px 18px 21px 24px 28px 32px 38px";
+
+        return $init_array;
+
+    }
+);
+add_filter(
+    'tiny_mce_before_init',
+    function( $initArray ) {
+        $initArray['font_formats'] = 'Space+Grotesk=Space+Grotesk;Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats';
+
+        return $initArray;
+    }
+);
+add_action(
+    'admin_init',
+    function() {
+        $font_url = 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap';
+
+        add_editor_style( str_replace( ',', '%2C', $font_url ) );
+    }
+);
+/**/
